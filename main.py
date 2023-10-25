c8b2f25c7d0f0dc6e44075beb5f2b996f664d2ea
StevenMODIMO <stevepetes26@gmail.com>
2023-10-25 21:25:44 +0300

    main.py File

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 
-df = pd.read_csv("adult.csv")
-
-plt.figure(figsize=(14,6))
-
-plt.title("Some Adult Shit Data...")
-
-sns.lineplot(data=df['salary'])
-
-plt.show()
