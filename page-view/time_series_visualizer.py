7957e2005007845f1e9c7e4f6d5661b734cb352b
StevenMODIMO <stevepetes26@gmail.com>
2023-11-05 07:40:11 +0300

    FCC

--- page-view/time_series_visualizer.py
+++ page-view/time_series_visualizer.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=['dates'], index_col='date')
+df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=['date'], index_col='date')
 
 # Clean data
 df = df[
@@ -32,12 +32,16 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    df_bar = df.groupby(['year', 'month'])['value'].mean()
+    df_bar = df_bar.unstack()
 
     # Draw bar plot
-
-
-
+    fig = df_bar.plot.bar(legend=True, figsize=(10,5), ylabel='Average Page Views', xlabel='Years').figure
+    plt.legend(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+    plt.xticks(fontsize=8)
+    plt.yticks(fontsize=8)
 
 
     # Save image and return fig (don't change this part)
@@ -52,8 +56,20 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    df_box['month_num'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('month_num')
 
+    fig,axes = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
+    axes[0]  = sns.boxplot(x=df_box['year'], y=df_box['value'], ax=axes[0])
+    axes[1]  = sns.boxplot(x=df_box['month'], y=df_box['value'], ax=axes[1])
 
+    axes[0].set_title("Year-wise Box Plot (Trend)")
+    axes[0].set_xlabel("Year")
+    axes[0].set_ylabel("Page Views")
+
+    axes[1].set_title("Month-wise Box Plot (Seasonality)")
+    axes[1].set_xlabel("Month")
+    axes[1].set_ylabel("Page Views")
 
 
 
